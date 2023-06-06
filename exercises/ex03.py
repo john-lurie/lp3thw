b952a3609476dbd76c8c71264f536e5872c6b4be
John C. Lurie <jlurie@protonmail.com>
2023-06-06 11:51:50 -0400

    Finished Exercise 3.

---
+++ exercises/ex03.py
@@ -0,0 +1,46 @@
+# Announce calculations of Hens and Roosters
+print("I will now count my chickens:")
+
+# Calculate number of Hens
+print("Hens", 25 + 30 / 6)
+# Calculate number of Roosters
+print("Roosters", 100 - 25 * 3 % 4)
+
+# Announce calculation of eggs
+print("Now I will count the eggs:")
+
+# Calculate number of eggs
+print(3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6)
+
+# Announce check of inequality
+print("Is it true that 3 + 2 < 5 - 7?")
+
+# Check if inequality is True
+print(3 + 2 < 5 - 7)
+
+# Do some basic arithmetic
+print("What is 3 + 2?", 3 + 2)
+print("What is 5 - 7?", 5 - 7)
+
+# State outcome of arithmetic
+print("Oh, that's why it's False.")
+
+# Announce next steps
+print("How about some more.")
+
+# Check truth of other inequalities
+print("Is it greater?", 5 > -2)
+print("Is it greater or equal?", 5 >= -2)
+print("Is it less or equal?", 5 <= -2)
+
+
+# Study Drill 3
+print()
+print("Study Drill 3: Calculate something else in Python.")
+print("Employees are paid every two weeks.")
+print("How many paychecks will they receive per month?")
+print("There are 26 paychecks in 12 months.")
+print("26 / 12 =", 26 // 12, "with a remainder of", 26 % 12)
+print("They will receive at least two paychecks every month.")
+print("And two months of the year they will receive a third paycheck.")
+ 
