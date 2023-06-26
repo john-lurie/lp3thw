fed55162055069b9f5ff8adcba81dda93157a54f
John C. Lurie <jlurie@protonmail.com>
2023-06-26 16:00:43 -0400

    Finished Exercise 30

---
+++ exercises/ex30.py
@@ -0,0 +1,32 @@
+people = 30
+cars = 40
+trucks = 15
+
+
+if cars > people:
+    print("We should take the cars.")
+elif cars < people:
+    print("We should not take the cars.")
+else:
+    print("We can't decide.")
+
+if trucks > cars:
+    print("That's too many trucks.")
+elif trucks < cars:
+    print("Maybe we could take the trucks.")
+else:
+    print("We still can't decide.")
+
+if people > trucks:
+    print("Alright, let's just take the trucks.")
+else:
+    print("Fine, let's stay home then.")
+
+# Study Drill 3: Try some more complex Boolean expressions.
+# This is not mathematically valid. Just an experiment.
+if people > cars and people > trucks:
+    print("We have too many people.")
+elif people < cars and people < trucks:
+    print("We don't have enough people.")
+else:
+    print("We have the right number of people.")
