459d2b77dd0a543204cd23f3f2b115bd1b9cd575
John C. Lurie <jlurie@protonmail.com>
2023-06-28 12:46:12 -0400

    Finished Exercise 34

---
+++ exercises/ex34.py
@@ -0,0 +1,59 @@
+animals = ['bear', 'python3.6', 'peacock', 'kangaroo', 'whale', 'platypus']
+
+print("""1. The animal at 1.
+The animal at 1 is the second animal and is a python3.6.
+The second animal is at 1 and is a python3.6.""")
+print("animals[1] =", animals[1])
+
+print("""\n2. The third animal.
+The third animal is at 2 and is a peacock.
+The animal at 2 is the third animal and is a peacock.""")
+print("animals[2] =", animals[2])
+
+print("""\n3. The first animal.
+The first animal is at 0 and is a bear.
+The animal at 0 is the first animal and is a bear.""")
+print("animals[0] =", animals[0])
+
+print("""\n4. The animal at 3.
+The animal at 3 is the fourth animal and is a kangaroo.
+The fourth animal is at 3 and is a kangaroo.""")
+print("animals[3] =", animals[3])
+
+print("""\n5. The fifth animal.
+The fifth animal is at 4 and is a whale.
+The animal at 4 is the fifth animal and is a whale.""")
+print("animals[4] =", animals[4])
+
+print("""\n6. The animal at 2.
+The animal at 2 is the third animal and is a peacock.
+The third animal is at 2 and is a peacock.""")
+print("animals[2] =", animals[2])
+
+print("""\n7. The sixth animal.
+The sixth animal is at 5 and is a platypus.
+The animal at 5 is the sixth animal and is a platypus.""")
+print("animals[5] =", animals[5])
+
+print("""\n8. The animal at 4.
+The animal at 4 is the fifth animal and is a whale.
+The fifth animal is at 4 and is a whale.""")
+print("animals[4] =", animals[4])
+
+
+# Study Drills 1: Years are ordinal numbers. There was no 0 BCE or 0 CE. 
+# 2010 CE is the two thousand and tenth year of the Common Era.
+
+# Study Drills 2 and 3: Write some more lists and work out similar indexes.
+trees = ['oak', 'beech', 'pine', 'pecan', 'magnolia']
+
+print("\nStudy Drills")
+print("""\nThe tree at 2.
+The tree at 2 is the third tree and is a pine.
+The third tree is at 2 and is a pine.""")
+print("trees[2] =", trees[2])
+
+print("""\nThe fifth tree.
+The fifth tree is at 4 and is a magnolia.
+The tree at 4 is the fifth tree and is a magnolia.""")
+print("trees[4] =", trees[4])
